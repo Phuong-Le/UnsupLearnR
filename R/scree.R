dc919af2a0e0ff18796c2dcddb0b5ff54fa72cad
phuong <alphn2015@gmail.com>
2022-11-28 13:04:47 +0000

    ENH: scree plot

---
+++ R/scree.R
@@ -0,0 +1,19 @@
+
+#' scree plot
+#' @import ggplot2
+#' @param pc_dataframe the dataframe of principal components
+#' @param pc_number numeric, the desired number of principal components
+#' @param cumulative whether to plot a normal scree plot or a cumulative scree plot
+#' @return a ggplot for scree plot
+#' @export
+scree_plot = function(pc_dataframe, pc_number=NULL, cumulative = T) {
+  y = ifelse(cumulative == T, cum_variance_proportion(pc_dataframe, pc_number=NULL), variance_proportion(pc_dataframe, pc_number=NULL))
+  x = ifelse(cumulative == T, 0:length(y), 1:length(y))
+  p = ggplot() +
+    aes(x=x,y=y,group=1) +
+    geom_line(colour = 'black') +
+    geom_point(colour = 'salmon') +
+    labs(x='principal component',y='percentage of variability explained') +
+    ggpubr::theme_pubr()
+  return(p)
+}
