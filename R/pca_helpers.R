44986b814b8ff6f336af4e0c7f8a57594df4d1e2
phuong <alphn2015@gmail.com>
2022-11-28 13:04:21 +0000

    ENH: pca helpers

---
+++ R/pca_helpers.R
@@ -0,0 +1,38 @@
+
+#' get the name of the PC given the PC number
+#' @param pc_number the number for the PC
+pc_name = function(pc_number) paste('PC',pc_number,sep = '',collapse='')
+
+
+#' Generates a vector of principal component names given the number of desired principle components
+#' @param pc_number numeric, the desired number of principal components
+#' @return a vector from PC1 to PCX where X is the number of desired PCs
+pc_names = function(pc_number) {
+  pc_names = c()
+  for (i in 1:pc_number) {
+    pc_names = append(pc_names,pc_name(i))
+  }
+  return(pc_names)}
+
+
+#' returns the variance for each principal component in pc_dataframe
+#' @param pc_dataframe the dataframe of principal components
+#' @param pc_number numeric, the desired number of principal components
+#' @return a vector of variance for each principal component
+#' @export
+variance_proportion = function(pc_dataframe,pc_number=NULL) {
+  pc_variance = c()
+  if (is.null(pc_number)) pc_number = ncol(pc_dataframe)
+  for (i in 1:pc_number) {
+    pc_variance = append(pc_variance,stats::var(pc_dataframe[names(pc_dataframe) == pc_names(pc_number)[i]]))
+  }
+  return(pc_variance*100/sum(pc_variance))}
+
+#' returns the variance for each principal component in pc_dataframe
+#' @param pc_dataframe the dataframe of principal components
+#' @param pc_number numeric, the desired number of principal components
+#' @return a vector of cumulative variance for each principal component
+#' @export
+cum_variance_proportion = function(pc_dataframe,pc_number=NULL) {
+  var_prop = variance_proportion(pc_dataframe,pc_number=NULL)
+  return(cumsum(c(0,var_prop)))}
---
+++ tests/testthat/test-pca_helpers.R
@@ -0,0 +1,22 @@
+test_that("pc_name() gives the correct pc names given a pc number", {
+  expect_equal(pc_name(2), 'PC2')
+})
+
+
+test_that("pc_names() gives the correct vector of pcs given a pc number", {
+  expect_equal(pc_names(3), c('PC1', 'PC2', 'PC3'))
+})
+
+
+test_that("variance_proportion() gives the variance of each PC given a dataframe", {
+  test_data = data.frame(PC1 = 1:4, PC2 = c(3,8,9,7), PC3 = c(3,0,2,3))
+  tot_var = var(test_data$PC1) + var(test_data$PC2) + var(test_data$PC3)
+  expect_equal(variance_proportion(test_data), c(var(test_data$PC1), var(test_data$PC2), var(test_data$PC3)) * 100 / tot_var)
+})
+
+test_that("cum_variance_proportion() gives the variance of each PC given a dataframe", {
+  test_data = data.frame(PC1 = 1:4, PC2 = c(3,8,9,7), PC3 = c(3,0,2,3))
+  var1 = var(test_data$PC1); var2 = var(test_data$PC2); var3 = var(test_data$PC3)
+  tot_var = var1 + var2 + var3
+  expect_equal(cum_variance_proportion(test_data), c(0, var1, var1+var2, var1 + var2 + var3) * 100 / tot_var)
+})
