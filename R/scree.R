09ff7ee3bd0d22bddc04631e7ad7353d3405c99a
phuong <alphn2015@gmail.com>
2022-11-29 17:03:54 +0000

    BUG: scree plot

--- R/scree.R
+++ R/scree.R
@@ -7,8 +7,12 @@
 #' @return a ggplot for scree plot
 #' @export
 scree_plot = function(pc_dataframe, pc_number=NULL, cumulative = T) {
-  y = ifelse(cumulative == T, cum_variance_proportion(pc_dataframe, pc_number=NULL), variance_proportion(pc_dataframe, pc_number=NULL))
-  x = ifelse(cumulative == T, 0:length(y), 1:length(y))
+  if (cumulative == T) {
+    y = c(0,cum_variance_proportion(pc_dataframe, pc_number=NULL))
+    x = 0:(length(y) - 1)} else
+    {
+      y = variance_proportion(pc_dataframe, pc_number=NULL)
+      x = 1:length(y)}
   p = ggplot() +
     aes(x=x,y=y,group=1) +
     geom_line(colour = 'black') +
